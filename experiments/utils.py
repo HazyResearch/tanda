4e04d26ad5863092b45d143e8765a2fb88b920d0
Henry Ehrenberg <henry.ehrenberg@outlook.com>
2018-04-25 12:44:26 -0700

    Fix #10

--- experiments/utils.py
+++ experiments/utils.py
@@ -52,8 +52,7 @@ def create_run_log(logdir, flags, name='run_log.json'):
     """Creates and saves initial run log including all flags"""
     log_dict = defaultdict(list)
     # Make sure flags are parsed, then use as initial log dict
-    flags._parse_flags()
-    log_dict.update(flags.__flags)
+    log_dict.update(flags.FLAGS)
     # Get the git commit hash
     log_dict['commit_hash'] = get_git_revision_short_hash()
     save_run_log(log_dict, logdir, name=name)
